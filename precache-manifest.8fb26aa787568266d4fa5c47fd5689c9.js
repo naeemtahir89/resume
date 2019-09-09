6bdb596c89fabde0ca43d06d85112edb10f3b133
naeemtahir89 <naeeem.tahir@gmail.com>
2019-09-10 00:02:26 +0500

    Updates

---
+++ precache-manifest.8fb26aa787568266d4fa5c47fd5689c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7acd7293eec54747a274e0b3bc0849ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b07f6e7e36b9fecc49e3",
+    "url": "/static/css/main.456ada25.chunk.css"
+  },
+  {
+    "revision": "1183d6864aa91724771e",
+    "url": "/static/js/2.2b8e9f2d.chunk.js"
+  },
+  {
+    "revision": "b07f6e7e36b9fecc49e3",
+    "url": "/static/js/main.8a9ac700.chunk.js"
+  },
+  {
+    "revision": "74c2f2482f5555d32d0a",
+    "url": "/static/js/runtime~main.90438f2f.js"
+  },
+  {
+    "revision": "55e871d173e69012a792e6ddae490da7",
+    "url": "/static/media/Pdf.55e871d1.pdf"
+  },
+  {
+    "revision": "a83ba6c15aaa7319bbc67c740e7e9de3",
+    "url": "/static/media/Resume.a83ba6c1.jpg"
+  }
+]);
